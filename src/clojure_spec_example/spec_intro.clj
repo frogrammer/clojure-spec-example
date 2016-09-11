8383b0e58df55dafa8d8f7cf6d90701b0ccf50f9
elenam <elenam@morris.umn.edu>
2016-09-11 15:42:48 -0500

    basic spec examples

--- src/clojure_spec_example/spec_intro.clj
+++ src/clojure_spec_example/spec_intro.clj
@@ -3,3 +3,22 @@
            [clojure.spec.test :as stest]))
 
 
+;;; specs with predicates: returns the value if it conforms to
+;;; a spec and a special keyword :clojure.spec/invalid if it doesn't
+(s/conform even? 1000)
+(s/conform even? 999)
+
+;; test predicates: the returned value is a boolean
+(s/valid? #(> % 5) 10)
+(s/valid? #(> % 5) 0)
+
+;; test set membership
+(s/valid? #{:club :diamond :heart :spade} :club)
+(s/valid? #{:club :diamond :heart :spade} 42)
+
+;; register specs in fully qualified namespaces
+(s/def ::suit #{:club :diamond :heart :spade})
+
+(s/explain ::suit 42)
+(s/explain-str ::suit 42)
+(s/explain-data ::suit 42)
